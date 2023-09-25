37148d1137113acc889e053a0cbb0356f346cc8b
FURKAN TURAL <furkanturalofficial@outlook.com>
2023-09-25 15:40:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string? folderPath;
+        Console.Write("Please write the console path correctly without misinformation (use file extention too): ");
+        folderPath = Console.ReadLine();
+        FileInfo fileInfo = new FileInfo(folderPath);
+        FileSecurity fileSecurity = fileInfo.GetAccessControl();
+        AuthorizationRuleCollection authorizationRuleCollection = fileSecurity.GetAccessRules(true, true, typeof(NTAccount));
+        foreach (FileSystemAccessRule rule in authorizationRuleCollection)
+        {
+            Console.WriteLine("Identity: {0}", rule.IdentityReference.Value);
+            Console.WriteLine("Type: {0}", rule.AccessControlType);
+            Console.WriteLine("Rights: {0}", rule.FileSystemRights);
+            Console.WriteLine("Inherited ACE: {0}", rule.IsInherited);
+            Console.WriteLine();
+        }
+    }
+}
